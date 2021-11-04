f89bdd6a7081631eee91fc8a5578b8aecc598ea7
Björn Tirsén <bjorntirsen@gmail.com>
2021-11-04 17:01:48 +0100

    :sparkles: Connect frontend to api

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -3,17 +3,13 @@ import { MongoClient } from 'mongodb';
 const handler = async (req, res) => {
   if (req.method === 'POST') {
     const data = req.body;
-
-    const client = MongoClient.connect(process.env.DB_URI);
+    const client = await MongoClient.connect(process.env.DB_URI);
     const db = client.db();
     const meetupsCollection = db.collection('meetups');
-
     const result = await meetupsCollection.insertOne(data);
-
-    console.log(result);
     client.close();
 
-    res.status(201).json({ message: 'Meetup inserted!' });
+    res.status(201).json({ message: 'Meetup inserted!', data: result });
   }
 };
 
