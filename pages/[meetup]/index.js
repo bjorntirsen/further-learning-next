febaf25a1764c7b9925a3abdb7a64111f42bc593
Björn Tirsén <bjorntirsen@gmail.com>
2021-11-04 14:30:40 +0100

    :sparkles: Add meetup page

---
+++ pages/[meetup]/index.js
@@ -0,0 +1,12 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+const MeetupDetailsPage = () => {
+  return (
+    <MeetupDetail
+      image='https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Frauenkirche_and_Neues_Rathaus_Munich_March_2013.JPG/1280px-Frauenkirche_and_Neues_Rathaus_Munich_March_2013.JPG'
+      title='First Meetup'
+    />
+  );
+};
+
+export default MeetupDetailsPage;
