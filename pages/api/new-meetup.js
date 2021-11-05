c19d2bd0b75eb9e59bf1d67cfffc3bda1e865a71
Björn Tirsén <bjorntirsen@gmail.com>
2021-11-05 08:54:17 +0100

    :sparkles: Add try catch block to api

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,16 +1,20 @@
 import { MongoClient } from 'mongodb';
 
 const handler = async (req, res) => {
-  if (req.method === 'POST') {
-    const data = req.body;
-    const client = await MongoClient.connect(process.env.DB_URI);
-    const db = client.db();
-    const meetupsCollection = db.collection('meetups');
-    const result = await meetupsCollection.insertOne(data);
-    console.log(result);
-    client.close();
+  try {
+    if (req.method === 'POST') {
+      const data = req.body;
+      const client = await MongoClient.connect(process.env.DB_URI);
+      const db = client.db();
+      const meetupsCollection = db.collection('meetups');
+      const result = await meetupsCollection.insertOne(data);
+      console.log(result);
+      client.close();
 
-    res.status(201).json({ message: 'Meetup inserted!' });
+      res.status(201).json({ message: 'Meetup inserted!' });
+    }
+  } catch (err) {
+    res.status(400).json({ error: err, errorMessage: err.message });
   }
 };
 
